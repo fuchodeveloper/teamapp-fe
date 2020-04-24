a2768080fa840a5465989086904db9692de538a5
fuchodeveloper <fmgbeoma@yahoo.com>
2020-04-24 16:36:05 +0100

    uri

--- client.ts
+++ client.ts
@@ -3,8 +3,12 @@ import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import ApolloClient from 'apollo-client';
 
+console.log('process', process.env);
+let appUrl = '';
+
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4001/graphql',
+  // uri: 'http://localhost:4001/graphql',
+  uri: 'https://teamapp-be.now.sh/graphl',
   credentials: 'include',
 });
 
