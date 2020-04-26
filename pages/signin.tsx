e2328eb47b5c1bf41162a32a0cb654bb20152571
fuchodeveloper <fmgbeoma@yahoo.com>
2020-04-26 11:59:29 +0100

    profile redirect

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -28,7 +28,7 @@ const SignIn = (props: any) => {
   if (loginStatus) {
     // cookie.set('signedin', 'true');
     Router.push('/profile');
-    window.location.href = '/profile';
+    // window.location.href = '/profile';
   }
 
   return (
